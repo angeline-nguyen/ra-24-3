54d22f744275784f39f5a36ac443c487ae7f4f32
angeline-nguyen <angeline.a.nguyen@gmail.com>
2025-08-14 06:24:57 -0700

    Added beforeAll and afterAll

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,17 @@
 const request = require('supertest');
 const app = require('../index.js');
 
+beforeAll(()=>{
+    server = app.listen(5000);
+});
+
 describe('GET /', () => {
     it('should return Hello World response for / route', async () => {
         const res = await request(app).get('/');
         expect(res.text).toEqual('Hello World!');
     });
+});
+
+afterAll(()=>{
+    server.close();
 });
